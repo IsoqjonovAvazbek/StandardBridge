1a19d86e8cdb4366b833e6adce0a4b7a5533e351
Avazbek-1 <isoqjonovavazbek@gmail.com>
2026-09-13 15:07:50 +0500

    Add privacy policy and terms of service pages

--- core/urls.py
+++ core/urls.py
@@ -86,6 +86,9 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('qms/', include('qms.urls')),
     path('expert-tools/', include('expert_tools.urls')),
+    path('privacy-policy/', TemplateView.as_view(template_name='legal/privacy_policy.html'), name='privacy_policy'),
+    path('terms-of-service/', TemplateView.as_view(template_name='legal/terms_of_service.html'), name='terms_of_service'),
+
 ]
 
 if settings.DEBUG:
